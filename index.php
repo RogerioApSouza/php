d5f4785e5dc363397bfad55d88105abf1946f9e5
rogerioapsouza <49996844+rogerioapsouza@users.noreply.github.com>
2021-04-08 11:01:29 -0300

    Variável de variável

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-
-	echo "Hello World!";
+	/*emplo de código utilizando variável declarada a partir 
+	  de outra variável
+	*/
+	echo "<h1>Hello World!</h1><br>";
+	$hello = "Hello";
+	$world = "world";
+	echo "<h1>$hello $world!</h1>";
+	$$hello = "Olá mundo!";
+	echo "<br>";
+	echo "$Hello";
 ?>
